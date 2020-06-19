29b87ac740a2af57e9447e3d354a80016dfde4ff
alexmedcalfbell <alexanderbell@outlook.com>
2020-06-19 16:20:51 +0100

    initial commit

---
+++ src/main/java/com/medcalfbell/superadventure/SuperAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.medcalfbell.superadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAdventureApplication.class, args);
+	}
+
+}
