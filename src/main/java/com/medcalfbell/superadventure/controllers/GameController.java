fe782a56afed6bf16848d644f5184a871d48b888
alexmedcalfbell <alexanderbell@outlook.com>
2020-06-22 17:29:50 +0100

    Added service

--- src/main/java/com/medcalfbell/superadventure/controllers/GameController.java
+++ src/main/java/com/medcalfbell/superadventure/controllers/GameController.java
@@ -1,5 +1,7 @@
 package com.medcalfbell.superadventure.controllers;
 
+import com.medcalfbell.superadventure.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/game")
 public class GameController {
 
+    private GameService gameService;
+
+    @Autowired
+    public GameController(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     @PostMapping("/action")
     public String processAction(@RequestBody String action) {
-        return action;
+        return gameService.processAction(action);
     }
 
 }
---
+++ src/main/java/com/medcalfbell/superadventure/services/GameService.java
@@ -0,0 +1,12 @@
+package com.medcalfbell.superadventure.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+    public String processAction(String command) {
+        return command;
+    }
+
+}
